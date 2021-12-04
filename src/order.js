c42f1dd788612f21c161de643de990c377e9d4c3
Ali Nooner <alinooner@gmail.com>
2021-12-04 16:16:22 -0600

    Add order and order test

--- src/order.js
+++ src/order.js
@@ -1,7 +1,40 @@
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
+}
+
+
+function takeOrder(order, deliveryOrders){
+  if(deliveryOrders.length<3){
+  deliveryOrders.push(order)
+  }
+      return takeOrder
+  }
+
+function refundOrder(orderNumber, deliveryOrders){
+  for (var i = 0; i<deliveryOrders.length; i++){
+    if (orderNumber === deliveryOrders[i].orderNumber)
+  deliveryOrders.splice(i,1)
+}
+return deliveryOrders
+}
+
+
+function listItems(orderList){
+  var tempList = []
+  for (var i=0; i<orderList.length; i++){
+    tempList.push(orderList[i].item)
+  }
+  return tempList.join(", ")
+}
+
+function searchOrder() {
+  if (deliveryOrders.indexOf(orders) !== -1)
+  return ("The order is not in the list!"){
+    else (deliveryOrders.indexOf(orders) == >1)
+    return ("The order is in the list!")
+  }
 }
